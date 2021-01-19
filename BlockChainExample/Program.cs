7c0f59e159899e71c918e9aea637dcdf7a02cf70
SteGa3 <s.gagni@live.it>
2021-01-19 21:35:48 +0100

    Initial check-in of module BlockChainExample

---
+++ BlockChainExample/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Newtonsoft.Json;
+using Entities;
+
+namespace BlockChainExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+            Blockchain phillyCoin = new Blockchain();
+            phillyCoin.AddBlock(new Block(DateTime.Now, null, "{sender:Henry,receiver:MaHesh,amount:10}"));
+            phillyCoin.AddBlock(new Block(DateTime.Now, null, "{sender:MaHesh,receiver:Henry,amount:5}"));
+            phillyCoin.AddBlock(new Block(DateTime.Now, null, "{sender:Mahesh,receiver:Henry,amount:5}"));
+
+            Console.WriteLine(JsonConvert.SerializeObject(phillyCoin, Formatting.Indented));
+        }
+    }
+}
